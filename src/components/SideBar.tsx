d84640258f5737983dadd4d1fcf9d8b0259873ed
flynn <flynn@dead.place>
2022-09-25 22:04:59 +0100

    Frontend: Improve Navigation

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,19 +1,47 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/router";
 
 const SideBar = () => {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  if (status === "unauthenticated") {
+    router.push("/public");
+  }
   return (
     <div className="flex flex-col bg-gray-800 w-28 h-screen">
       <Link href="/">
-        <div className="flex justify-center w-full py-12 border-slate-500/60 border cursor-pointer">
+        <div className="flex justify-center w-full py-12 border-slate-500/60 border cursor-pointer hover:bg-gray-600">
           <a className="text-white">Home</a>
         </div>
       </Link>
-      <Link href="/you">
-        <div className="flex justify-center w-full py-12 border-slate-500/60 border cursor-pointer">
-          <a className="text-white">Profile</a>
+      <Link href={"/user/" + session?.user?.id}>
+        <div className="flex flex-col w-full py-6 border-slate-500/60 border cursor-pointer hover:bg-gray-600">
+          <div className="flex flex-col justify-center items-center w-full">
+            <img
+              className="w-12 h-12 rounded-full"
+              src={session?.user?.image as string}
+            />
+            <a className="text-white pt-2">Timeline</a>
+          </div>
         </div>
       </Link>
+      {session ? (
+        <>
+          <button className="text-white" onClick={() => signOut()}>
+            <div className="flex justify-center w-full py-12 border-slate-500/60 border cursor-pointer hover:bg-gray-600">
+              Sign Out
+            </div>
+          </button>
+        </>
+      ) : null}
+      {status === "loading" ? (
+        <div className="flex justify-center w-full py-12 border-slate-500/60 border cursor-pointer hover:bg-gray-600">
+          <a className="text-white">Loading...</a>
+        </div>
+      ) : null}
     </div>
   );
 };
---
+++ src/pages/public.tsx
@@ -0,0 +1,37 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/router";
+
+const Public = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
+  return (
+    <>
+      <Head>
+        <title>fuckthebirdapp</title>
+      </Head>
+
+      <div className="flex flex-col h-screen w-full md:p-4 gap-9">
+        <div className="flex flex-col overflow-auto h-screen justify-center items-center p-24 bg-gray-200">
+          <h1>
+            Not signed in. Sign in{" "}
+            <Link href="/api/auth/signin">
+              <a className="hover:underline text-sky-400">here</a>
+            </Link>
+            .
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Public;
