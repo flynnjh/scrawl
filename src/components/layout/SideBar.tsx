e87473efd9c77dc6798e443545d46e23832db84d
flynn <flynn@dead.place>
2022-10-03 17:25:57 +0100

    Frontend: Add Sign Out Button to SideBar

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,10 +1,13 @@
-import { MdBookmark, MdHome } from "react-icons/md";
-import React, { ReactNode } from "react";
+import {
+  MdBookmark,
+  MdHome,
+  MdOutlineExitToApp,
+  MdPerson,
+} from "react-icons/md";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-import { IconButton } from "@material-tailwind/react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { ReactNode } from "react";
 
 interface Props {
   children?: ReactNode;
@@ -26,7 +29,7 @@ const SButton = ({ children, ...props }: Props) => {
 const SideBar = () => {
   const { data: session, status } = useSession();
   return (
-    <div className="flex flex-col w-28 lg:w-96 md:w-64 overflow-auto h-screen shadow-md">
+    <div className="flex flex-col w-28 lg:w-96 md:w-64 overflow-auto h-screen shadow-md bg-blue-50/20">
       <div className="flex justify-center items-center py-16">
         <h1 className="text-black md:text-6xl text-4xl">🔏</h1>
       </div>
@@ -36,10 +39,7 @@ const SideBar = () => {
           <a className="text-black lg:pl-4 md:text-xl">Home</a>
         </SButton>
         <SButton link={"/user/" + session?.user?.id}>
-          <img
-            className="w-12 h-12 rounded-full"
-            src={session?.user?.image as string}
-          />
+          <MdPerson className="w-12 h-12" />
           <a className="text-black lg:pl-4 md:text-xl">Timeline</a>
         </SButton>
         <SButton link={"/404"}>
@@ -47,13 +47,12 @@ const SideBar = () => {
           <a className="text-black lg:pl-4 md:text-xl">Bookmarks</a>
         </SButton>
         {session ? (
-          <>
-            <button className="text-black mt-auto" onClick={() => signOut()}>
-              <div className="flex md:max-h-10 justify-center items-center text-xl w-full py-12 cursor-pointer hover:bg-gray-600">
-                Sign Out
-              </div>
-            </button>
-          </>
+          <button className="mt-auto" onClick={() => signOut()}>
+            <SButton>
+              <MdOutlineExitToApp className="w-12 h-12" />
+              <a className="text-black lg:pl-4 md:text-xl">Sign Out</a>
+            </SButton>
+          </button>
         ) : null}
         {status === "loading" ? (
           <div className="flex justify-center w-full py-12 border-slate-500/60 border cursor-pointer hover:bg-gray-600">
