8608e0e7326827373d9bbd271756b1063ae3ef6e
flynn <flynn@dead.place>
2022-10-08 19:14:51 +0100

    Frontend: Implemented TabBar on Mobile :^)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,6 +3,8 @@ import React, { ReactNode } from "react";
 import AuthContext from "../../context/AuthContext";
 import Head from "next/head";
 import SideBar from "./SideBar";
+import TabBar from "./TabBar";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 interface Props {
   children?: ReactNode;
@@ -11,15 +13,17 @@ interface Props {
 // FIXME: Added AuthContext to Layout for the time being, this way routes that utilise it will be protected.
 
 const Layout = ({ children, ...props }: Props) => {
+  const isMobile = useMediaQuery(550);
   return (
     <AuthContext>
       <Head>
         <title>Scrawl</title>
       </Head>
       <main className="flex flex-row w-screen h-screen">
-        <SideBar />
+        {!isMobile ? <SideBar /> : null}
         <div className="flex flex-col h-screen w-full md:p-4 md:gap-9">
           {children}
+          {isMobile ? <TabBar /> : null}
         </div>
       </main>
     </AuthContext>
---
+++ src/components/layout/TabBar.tsx
@@ -0,0 +1,59 @@
+import { FiBook, FiBookmark, FiHome, FiUser } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import Link from "next/link";
+import { MdOutlineExitToApp } from "react-icons/md";
+import { ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode;
+  link?: ReactNode;
+}
+
+const SButton = ({ children, ...props }: Props) => {
+  return (
+    <Link href={"" + props.link}>
+      <div className="flex lg:flex-row flex-col w-full py-6 cursor-pointer hover:bg-gray-600">
+        <div className="flex lg:flex-row flex-col lg:justify-start justify-center lg:pl-8 lg:h-10 items-center w-full">
+          {children}
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+const TabBar = () => {
+  const { data: session, status } = useSession();
+  return (
+    <div className="flex px-6 flex-row w-full">
+      <div className="flex justify-center items-center">
+        <h1 className="text-black md:text-6xl text-4xl">🔏</h1>
+      </div>
+      <div className="flex flex-row ml-auto gap-9 h-full">
+        <SButton link={"/"}>
+          <FiHome className="w-8 h-8" />
+        </SButton>
+        <SButton link={"/user/" + session?.user?.id}>
+          <FiUser className="w-8 h-8" />
+        </SButton>
+        <SButton link={"/"}>
+          <FiBookmark className="w-8 h-8" />
+        </SButton>
+        {session ? (
+          <div className="mt-auto">
+            <SButton link={"/api/auth/signout?csrf=true"}>
+              <MdOutlineExitToApp className="w-8 h-8" />
+            </SButton>
+          </div>
+        ) : null}
+        {status === "loading" ? (
+          <SButton>
+            <a className="text-black">Loading...</a>
+          </SButton>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default TabBar;
