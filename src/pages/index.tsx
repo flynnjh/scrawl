6db63c988dd39c8cc892426b464cf9948ebda731
flynn <flynn@dead.place>
2022-10-15 22:25:09 +0100

    Frontend: Update /index to properly redirect to the proper routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
+import AuthContext from "../context/AuthContext";
+import { ClipLoader } from "react-spinners";
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
@@ -9,19 +11,34 @@ const Public = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
 
-  if (status === "authenticated") {
-    router.push("/home");
-  }
-
   // This will be replaced soon.
 
-  return (
-    <div className="flex flex-col overflow-auto h-screen justify-center items-center p-2">
-      <Link href="/api/auth/signin">
-        <a className="hover:underline text-blue-400 text-4xl">Sign In</a>
-      </Link>
-    </div>
-  );
+  if (status === "authenticated") {
+    router.push("/home");
+    return (
+      <div className="flex h-screen w-full justify-center items-center text-2xl bg-blue-gray-50/70">
+        <ClipLoader color="#42A5F5" size={50} />
+      </div>
+    );
+  }
+
+  if (status === "loading") {
+    return (
+      <div className="flex h-screen w-full justify-center items-center text-2xl bg-blue-gray-50/70">
+        <ClipLoader color="#42A5F5" size={50} />
+      </div>
+    );
+  }
+
+  if (status === "unauthenticated") {
+    return (
+      <div className="flex flex-col overflow-auto h-screen justify-center items-center p-2">
+        <Link href="/api/auth/signin">
+          <a className="hover:underline text-blue-400 text-4xl">Sign In</a>
+        </Link>
+      </div>
+    );
+  }
 };
 
 export default Public;
