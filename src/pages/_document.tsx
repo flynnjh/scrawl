7d0c205a460f2635665276b5648fc1e019580d02
flynn <flynn@dead.place>
2022-10-24 15:22:25 +0100

    Meta: Repalce fav.farm favicon with a replacement made in Photoshop

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,7 @@ const Document: NextPage = () => {
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,200;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="https://fav.farm/🔏" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="bg-blue-gray-50/70">
         <Main />
