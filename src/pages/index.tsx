ac7474868c89b2f107c84f426d69377c3588be7f
flynn <flynn@dead.place>
2022-10-28 21:37:21 +0100

    feat: implement landing page :^)
    writing the privacy policy and some minor styling changes should be the last steps :,^)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
+import { AiOutlineGithub } from "react-icons/ai";
 import { ClipLoader } from "react-spinners";
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
+import useMediaQuery from "../hooks/useMediaQuery";
 import { useRouter } from "next/router";
 
 const Public = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
+  const isMobile = useMediaQuery(550);
 
   // This will be replaced soon.
+  // TODO: Write privacy policy.
 
   if (status === "authenticated") {
     router.push("/home");
@@ -31,10 +35,58 @@ const Public = () => {
 
   if (status === "unauthenticated") {
     return (
-      <div className="flex flex-col overflow-auto h-screen justify-center items-center p-2">
-        <Link href="/api/auth/signin">
+      <div className="flex flex-col overflow-auto h-screen bg-[#fbfbfc]">
+        <Head>
+          <title>Scrawl</title>
+        </Head>
+        <main className="flex flex-col h-full w-full justify-center items-center p-12">
+          <p className="text-9xl py-4" aria-hidden>
+            🔏
+          </p>
+          <h1 className="text-2xl pb-4">Scrawl</h1>
+          <p className="text-lg text-center">
+            Scrawl is a safe and private space for you to store your thoughts.
+            No ads, data collections, likes or retweets.
+          </p>
+          <p className="text-lg text-center pb-8">
+            Just you and what you're thinking.
+          </p>
+          <button
+            className="flex flex-row rounded-md items-center bg-black text-white px-9 py-2"
+            onClick={() => {
+              signIn();
+            }}
+          >
+            <AiOutlineGithub className="w-8 h-8 " />
+            <h1 className="pl-4">Sign in with GitHub</h1>
+          </button>
+        </main>
+        <footer className="p-9 mt-auto">
+          <div className="flex flex-row items-center">
+            <AiOutlineGithub
+              className="w-8 h-8 fill-gray-800 hover:cursor-pointer"
+              onClick={() => {
+                router.push("https://github.com/flynnjh/scrawl");
+              }}
+            />
+            <Link href="https://github.com/flynnjh/scrawl">
+              <a className="text-gray-800 hover:underline pl-4">
+                {!isMobile && "View the source code on GitHub."}
+              </a>
+            </Link>
+            <Link href="https://twitter.com/distantsynths">
+              <a className="text-gray-800 hover:underline ml-auto">Twitter</a>
+            </Link>
+            <Link href="https://twitter.com/distantsynths">
+              <a className="text-gray-800 hover:underline ml-4">
+                Privacy Policy (foo)
+              </a>
+            </Link>
+          </div>
+        </footer>
+        {/* <Link href="/api/auth/signin">
           <a className="hover:underline text-blue-400 text-4xl">Sign In</a>
-        </Link>
+        </Link> */}
       </div>
     );
   }
